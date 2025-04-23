e4bea49ae2a2e5d9e4885783ece95e85686a3f90
Reykel Raflen Awang <reykel210618@gmail.com>
2025-04-23 11:53:08 +0700

    tambah route

--- PBL-Kel2/routes/web.php
+++ PBL-Kel2/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
@@ -9,16 +8,3 @@ Route::get('/', function () {
 });
 
 Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-=======
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
->>>>>>> 4e97a89dd2d2ca1d6b109ccbc8b5bf5206b02042
