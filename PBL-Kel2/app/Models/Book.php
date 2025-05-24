e1af3c4d4967dc1d5d482ca8e620442c7341286a
Reykel Raflen Awang <reykel210618@gmail.com>
2025-05-24 20:17:50 +0700

    feat: menambah katalog buku

--- PBL-Kel2/app/Models/Book.php
+++ PBL-Kel2/app/Models/Book.php
@@ -1,7 +1,5 @@
 <?php
 
-// app/Models/Book.php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,6 +10,63 @@ class Book extends Model
     use HasFactory;
 
     protected $fillable = [
-        'title', 'author', 'cover', 'original_price', 'discount_price'
+        'title',
+        'author',
+        'description',
+        'cover',
+        'original_price',
+        'discount_price',
+        'ebook_price'
     ];
+
+    protected $casts = [
+        'original_price' => 'integer',
+        'discount_price' => 'integer',
+        'ebook_price' => 'integer',
+    ];
+
+    // Accessor untuk format harga
+    public function getFormattedOriginalPriceAttribute()
+    {
+        return 'Rp ' . number_format($this->original_price, 0, ',', '.');
+    }
+
+    public function getFormattedDiscountPriceAttribute()
+    {
+        return 'Rp ' . number_format($this->discount_price, 0, ',', '.');
+    }
+
+    public function getFormattedEbookPriceAttribute()
+    {
+        return 'Rp ' . number_format($this->ebook_price, 0, ',', '.');
+    }
+
+    // Accessor untuk menghitung persentase diskon
+    public function getDiscountPercentageAttribute()
+    {
+        if ($this->original_price > 0) {
+            return round((($this->original_price - $this->discount_price) / $this->original_price) * 100);
+        }
+        return 0;
+    }
+
+    // Accessor untuk menghitung total hemat
+    public function getSavingsAmountAttribute()
+    {
+        return $this->original_price - $this->discount_price;
+    }
+
+    public function getFormattedSavingsAmountAttribute()
+    {
+        return 'Rp ' . number_format($this->savings_amount, 0, ',', '.');
+    }
+
+    // Accessor untuk deskripsi yang dipotong
+    public function getShortDescriptionAttribute()
+    {
+        if (strlen($this->description) > 100) {
+            return substr($this->description, 0, 100) . '...';
+        }
+        return $this->description;
+    }
 }
